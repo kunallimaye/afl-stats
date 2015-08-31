da421a48c3c48862e35948bab1be978e2739b70e
Kunal Limaye <kunallimaye@gmail.com>
2015-08-31 23:27:50 +1000

    Adding a basic route

---
+++ afl-stats-extractor/src/main/java/me/finiteloop/afl/stats/extractor/ExtractRoute.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package me.finiteloop.afl.stats.extractor;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * @author klimaye
+ *
+ */
+public class ExtractRoute extends RouteBuilder {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new ExtractRoute());
+		context.start();
+	}
+
+	@Override
+	public void configure() throws Exception {
+		from("file:src/data")
+			.log("File is: " + simple("${header.CamelFileName}"));
+		
+	}
+
+}
